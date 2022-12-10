dd9a5510de6ce92f99c0de118913da464e3e3753
Mahmood Ali <maali@gn.com>
2022-12-10 07:53:48 +0100

    Add project files.

---
+++ TrackPlanTest/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using TrackPlanTest.Models;
+
+namespace TrackPlanTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DiagonalRow(TrackPlanModel model)
+        {
+            return (ModelState.IsValid) ? View(model) : View(string.Empty);
+        }
+    }
+}
---
+++ TrackPlanTest/Models/TrackPlanModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrackPlanTest.Models
+{
+    public class TrackPlanModel
+    {
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please enter your name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
